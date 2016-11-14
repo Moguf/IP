9864b030dc54b1f7f193170a54f31aef0bd52570
Mogu programming <kbu94984@gmail.com>
2016-11-14 22:30:23 +0900

    MOD: ch4.

---
+++ cpp/ch4/includes/prewitt.hpp
@@ -0,0 +1,9 @@
+#ifndef __PREWITT_HPP
+#define __PREWITT_HPP
+#include "Params.h"
+void prewitt(unsigned char image_in[Y_SIZE][X_SIZE],
+              unsigned char image_out[Y_SIZE][X_SIZE],
+             double amp);
+#endif
+
+
